54313d64c9af2d2c807664db6ef1047d73087c93
sscg13 <iamdumbybear1@gmail.com>
2024-09-17 10:33:36 +0800

    bench: 1243907

--- src/search.cpp
+++ src/search.cpp
@@ -759,7 +759,7 @@ Value Search::Worker::search(
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha && std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
-            return value;
+            return value + (alpha - value) / 4;
     }
 
     // Step 8. Futility pruning: child node (~40 Elo)
