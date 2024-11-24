df165fac1f350e1bd0a5d21b87815f5193f4e031
sscg13 <iamdumbybear1@gmail.com>
2024-11-24 09:11:44 +0800

    bench: 1039837

--- src/search.cpp
+++ src/search.cpp
@@ -789,7 +789,7 @@ Value Search::Worker::search(
     // Step 8. Futility pruning: child node (~40 Elo)
     // The depth condition is important for mate finding.
     if (!ss->ttPv && depth < 14
-        && eval - futility_margin(depth, cutNode && !ss->ttHit, improving, opponentWorsening)
+        && eval - futility_margin(depth + !ss->ttHit, cutNode && !ss->ttHit, improving, opponentWorsening)
                - (ss - 1)->statScore / 290
              >= beta
         && eval >= beta && (!ttData.move || ttCapture) && beta > VALUE_TB_LOSS_IN_MAX_PLY
