aac087f2558295fbec590b9b67038e606f905e6f
sscg13 <iamdumbybear1@gmail.com>
2024-12-23 07:34:37 +0800

    bench: 943413

--- src/search.cpp
+++ src/search.cpp
@@ -70,7 +70,7 @@ Value futility_margin(Depth d, bool noTtCutNode, bool improving, bool oppWorseni
     Value improvingDeduction = improving * futilityMult * 2;
     Value worseningDeduction = oppWorsening * futilityMult / 3;
 
-    return futilityMult * d - improvingDeduction - worseningDeduction;
+    return futilityMult * d - improvingDeduction - !improving * worseningDeduction;
 }
 
 constexpr int futility_move_count(bool improving, Depth depth) {
