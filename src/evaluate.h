8b28eb238198a3fd0593815365dd4f60a1bc1b94
sscg13 <iamdumbybear1@gmail.com>
2026-05-18 16:53:45 -0700

    nnue-pytorch net
    bench: 2547725

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,7 +33,7 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultName "nn-6a5611fa8043.nnue"
+#define EvalFileDefaultName "nn-a99b16222903.nnue"
 
 namespace NNUE {
 class Network;
